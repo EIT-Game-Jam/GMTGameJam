5f4c96cefeb3d93e51de2fcd3087b3330d96d086
GusThomsen <109357013+GusThomsen@users.noreply.github.com>
2022-07-16 02:31:58 +0200

    two characters added
    two characters added with controls and movement

--- GMTK22/Assets/Scripts/PlayerMovement.cs
+++ GMTK22/Assets/Scripts/PlayerMovement.cs
@@ -9,20 +9,24 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
 
     [SerializeField] private Rigidbody2D rb;
-
+    public Animator animator;
 
     Vector2 movement;
 
 
     public void OnMove(InputAction.CallbackContext context)
     {
-         movement = context.ReadValue<Vector2>();
+        movement = context.ReadValue<Vector2>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         //input
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);
     }
 
 
