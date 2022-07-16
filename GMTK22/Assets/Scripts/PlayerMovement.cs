2c30aa4bb776e584f965e3207cd9690fa81d8df1
GusThomsen <109357013+GusThomsen@users.noreply.github.com>
2022-07-16 18:27:48 +0200

    TwoPlayer shifting

--- GMTK22/Assets/Scripts/PlayerMovement.cs
+++ GMTK22/Assets/Scripts/PlayerMovement.cs
@@ -6,27 +6,59 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public GameObject otherPlayer;
+    private bool activePlayer = false;
     [SerializeField] private float moveSpeed = 5f;
 
     [SerializeField] private Rigidbody2D rb;
     public Animator animator;
 
     Vector2 movement;
+    private bool used = false;
+    private bool shift = false;
+    public bool ifTwoPlayer = false;
 
 
     public void OnMove(InputAction.CallbackContext context)
     {
         movement = context.ReadValue<Vector2>();
+        Debug.Log(movement);
+
+    }
+
+    public void OnUse(InputAction.CallbackContext context)
+    {
         
+    }
+
+    public void OnShift(InputAction.CallbackContext context)
+    {
+        if(ifTwoPlayer == false)
+        {
+        shift = context.action.triggered;
+        if(shift){
+            otherPlayer.GetComponent<PlayerMovement>().enabled = true;
+            GetComponent<PlayerMovement>().enabled = false;
+            
+        }
+        }
+    }
+
+
+    public void TwoPlayer(InputAction.CallbackContext context)
+    {
+      
     }
 
     // Update is called once per frame
     void Update()
     {
         //input
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.magnitude);
+        /*
+       animator.SetFloat("Horizontal", movement.x);
+       animator.SetFloat("Vertical", movement.y);
+       animator.SetFloat("Speed", movement.magnitude);
+       */
     }
 
 
