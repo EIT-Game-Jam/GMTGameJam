c27ad58ba4410164f8e680b6d7852084347f3ca3
MalVM <mavaerumm@protonmail.com>
2022-07-16 17:45:33 +0200

    UI Settings

---
+++ GMTK22/Assets/Settings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    private void start() 
+    {
+        transform.localScale = Vector2.zero;
+    }
+    public void open()
+    {
+        transform.LeanScale(Vector2.one, 0.8f);
+    }
+    public void close()
+    {
+        transform.LeanScale(Vector2.zero, 1f).setEaseInBack();
+    }
+}
